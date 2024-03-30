891804f1f9d1e0570cc24754a56e5b5e1df74c07
Gustavo Alexandre Cardoso de Sousa <gustavoacs2000@gmail.com>
2024-03-30 17:22:19 -0300

    implementando chamadas no app

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 import express from 'express';
-import config  from 'dotenv/config';
+import config from 'dotenv/config';
 import { router } from './routes/homePage.js';
-import exphbs from 'express-handlebars';
-
+import { engine } from 'express-handlebars';
 const app = express();
-
 app.use(
     express.urlencoded({
-        extended:true
+        extended: true
     }),
     express.json()
 );
 
-app.engine('handlebars', exphbs());
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
-app.use('/',router);
+app.use('/', router);
 
-app.listen(process.env.ROUTE)
+app.listen(process.env.ROUTE, (req, res) => {
+    console.log(`Server listening on port ${process.env.ROUTE}`)
+})
